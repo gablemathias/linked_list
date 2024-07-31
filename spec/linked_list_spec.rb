ce2f8b1631c46566ecf771eab19124d12d90f42a
Gabriel Mathias <gablemathias@icloud.com>
2024-07-30 21:11:12 -0300

    add test cases(to be filled)

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -9,7 +9,23 @@ RSpec.describe LinkedList do
       expect(result.value).to eq('node3')
     end
 
-    xit 'Adds a new node to the beginning of the list' do
-    end
+    it 'Adds a new node to the beginning of the list'
   end
+
+  context 'When iterating the list' do
+    it 'Checks the size of the list'
+    it 'Returns the first node of the list'
+    it 'Returns the last node of the list'
+    it 'Returns the node at given index'
+    it 'Checks if a value is in the list'
+    it 'Returns a value from the list'
+  end
+
+  context 'When modifying the list' do
+    it 'Removes the last element of the list'
+    it 'Inserts a new node at a given index'
+    it 'Removes a node at the given index'
+  end
+
+  it 'Prints the LinkedList objects as a String'
 end
